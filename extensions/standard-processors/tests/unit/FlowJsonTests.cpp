apache/nifi-minifi-cpp

MINIFICPP-2502 Add processorBulletins C2 metric node to FlowInformation

extensions/standard-processors/tests/unit/FlowJsonTests.cpp
@@ -74,6 +74,7 @@ TEST_CASE("NiFi flow json format is correctly parsed") {
       "schedulingPeriod": "3 sec",
       "penaltyDuration": "12 sec",
       "yieldDuration": "4 sec",
+      "bulletinLevel": "ERROR",
       "runDurationMillis": 12,
       "autoTerminatedRelationships": ["one", "two"],
       "properties": {
@@ -143,6 +144,7 @@ TEST_CASE("NiFi flow json format is correctly parsed") {
   REQUIRE(3s == proc->getSchedulingPeriod());
   REQUIRE(12s == proc->getPenalizationPeriod());
   REQUIRE(4s == proc->getYieldPeriod());
+  REQUIRE(proc->getLogBulletinLevel() == logging::LOG_LEVEL::err);
   REQUIRE(proc->isAutoTerminated({"one", ""}));
   REQUIRE(proc->isAutoTerminated({"two", ""}));
   REQUIRE_FALSE(proc->isAutoTerminated({"three", ""}));

EchoCog/minifi-cpp

extensions/standard-processors/tests/unit/FlowJsonTests.cpp
@@ -79,6 +79,7 @@ TEST_CASE("NiFi flow json format is correctly parsed") {
       "schedulingPeriod": "3 sec",
       "penaltyDuration": "12 sec",
       "yieldDuration": "4 sec",
+      "bulletinLevel": "ERROR",
       "runDurationMillis": 12,
       "autoTerminatedRelationships": ["one", "two"],
       "properties": {
@@ -148,6 +149,7 @@ TEST_CASE("NiFi flow json format is correctly parsed") {
   CHECK(3s == proc->getSchedulingPeriod());
   CHECK(12s == proc->getPenalizationPeriod());
   CHECK(4s == proc->getYieldPeriod());
+  CHECK(proc->getLogBulletinLevel() == logging::LOG_LEVEL::err);
   CHECK(proc->isAutoTerminated({"one", ""}));
   CHECK(proc->isAutoTerminated({"two", ""}));
   CHECK_FALSE(proc->isAutoTerminated({"three", ""}));