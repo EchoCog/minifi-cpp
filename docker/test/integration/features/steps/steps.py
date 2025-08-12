lordgamez/nifi-minifi-cpp

MINIFICPP-2587 Add Sparkplug support for ConsumeMQTT processor
- Extract protobuf library to be built separately
- Upgrade grpc library to v1.72.2
- Change RecordReader interface to expect InputStream instead of
  FlowFile and ProcessSession
- Add SparkplugReader record reader controller
- Add Record Reader and Record Writer properties to ConsumeMQTT
  processor
- Add "Add Attributes As Fields" to add flow file attributes as record
  fields if records are used in ConsumeMQTT
- Make ConsumeMQTT trigger unit testable
- Add new attributes to ConsumeMQTT created flow files to match NiFi
- Implement record reader and writer usage in ConsumeMQTT

docker/test/integration/features/steps/steps.py
@@ -26,6 +26,7 @@ from minifi.controllers.KubernetesControllerService import KubernetesControllerS
 from minifi.controllers.JsonRecordSetWriter import JsonRecordSetWriter
 from minifi.controllers.JsonTreeReader import JsonTreeReader
 from minifi.controllers.CouchbaseClusterService import CouchbaseClusterService
+from minifi.controllers.SparkplugBReader import SparkplugBReader
 
 from behave import given, then, when
 from behave.model_describe import ModelDescriptor
@@ -433,6 +434,18 @@ def step_impl(context, processor_name, service_property_name, property_name, pro
 def step_impl(context):
     context.test.acquire_container(context=context, name="mqtt-broker", engine="mqtt-broker")
     context.test.start('mqtt-broker')
+
+
+@given("a SparkplugBReader controller service is set up")
+def step_impl(context):
+    json_record_set_reader = SparkplugBReader("SparkplugBReader")
+    container = context.test.acquire_container(context=context, name="minifi-cpp-flow")
+    container.add_controller(json_record_set_reader)
+
+
+@when("a test Sparkplug payload is published to the topic \"{topic}\"")
+def step_impl(context, topic):
+    context.test.publish_test_sparkplug_payload(topic)
 
 
 # s3 setup

EchoCog/minifi-cpp

docker/test/integration/features/steps/steps.py
@@ -28,6 +28,7 @@ from minifi.controllers.JsonTreeReader import JsonTreeReader
 from minifi.controllers.XMLReader import XMLReader
 from minifi.controllers.XMLRecordSetWriter import XMLRecordSetWriter
 from minifi.controllers.CouchbaseClusterService import CouchbaseClusterService
+from minifi.controllers.SparkplugBReader import SparkplugBReader
 
 from behave import given, then, when
 from behave.model_describe import ModelDescriptor
@@ -465,6 +466,18 @@ def step_impl(context, processor_name, service_property_name, property_name, pro
 def step_impl(context):
     context.test.acquire_container(context=context, name="mqtt-broker", engine="mqtt-broker")
     context.test.start('mqtt-broker')
+
+
+@given("a SparkplugBReader controller service is set up")
+def step_impl(context):
+    sparkplug_record_set_reader = SparkplugBReader("SparkplugBReader")
+    container = context.test.acquire_container(context=context, name="minifi-cpp-flow")
+    container.add_controller(sparkplug_record_set_reader)
+
+
+@when("a test Sparkplug payload is published to the topic \"{topic}\"")
+def step_impl(context, topic):
+    context.test.publish_test_sparkplug_payload(topic)
 
 
 # s3 setup