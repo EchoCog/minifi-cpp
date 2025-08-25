apache/nifi-minifi-cpp

MINIFICPP-2558 Revamp site to site communication
- Fix communicating with remote output port
- Add handling of remote port batch size properties
- Extend port management in config generation in docker integration tests
- Make input port in remote process groups optional (as using only output ports can also be a valid configuration)
- Add additional testing
- Refactor RPG and site to site clients
- Additional documentation

libminifi/include/RemoteProcessGroupPort.h
@@ -1,6 +1,6 @@
 /**
- * @file RemoteProcessorGroupPort.h
- * RemoteProcessorGroupPort class declaration
+ * @file RemoteProcessGroupPort.h
+ * RemoteProcessGroupPort class declaration
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -51,7 +51,7 @@ namespace org::apache::nifi::minifi {
 class RPGLatch {
  public:
   RPGLatch(bool increment = true) { // NOLINT
-    static std::atomic<int> latch_count(0);
+    static std::atomic<int64_t> latch_count(0);
     count = &latch_count;
     if (increment)
       count++;
@@ -61,39 +61,35 @@ class RPGLatch {
     count--;
   }
 
-  int getCount() {
+  int64_t getCount() {
     return *count;
   }
 
  private:
-  std::atomic<int> *count;
+  std::atomic<int64_t> *count;
 };
 
 struct RPG {
-  std::string host_;
-  int port_;
-  std::string protocol_;
+  std::string host;
+  int port;
+  std::string protocol;
 };
 
-class RemoteProcessorGroupPort : public core::ProcessorImpl {
+class RemoteProcessGroupPort : public core::ProcessorImpl {
  public:
-  RemoteProcessorGroupPort(std::string_view name, std::string url, std::shared_ptr<Configure> configure, const utils::Identifier &uuid = {})
+  RemoteProcessGroupPort(std::string_view name, std::string url, std::shared_ptr<Configure> configure, const utils::Identifier &uuid = {})
       : core::ProcessorImpl(name, uuid),
         configure_(std::move(configure)),
-        direction_(sitetosite::SEND),
+        direction_(sitetosite::TransferDirection::SEND),
         transmitting_(false),
-        timeout_(0),
-        bypass_rest_api_(false),
-        ssl_service(nullptr),
-        logger_(core::logging::LoggerFactory<RemoteProcessorGroupPort>::getLogger(uuid)) {
-    client_type_ = sitetosite::CLIENT_TYPE::RAW;
-    protocol_uuid_ = uuid;
-    site2site_secure_ = false;
-    peer_index_ = -1;
+        protocol_uuid_(uuid),
+        client_type_(sitetosite::ClientType::RAW),
+        peer_index_(-1),
+        logger_(core::logging::LoggerFactory<RemoteProcessGroupPort>::getLogger(uuid)) {
     // REST API port and host
     setURL(std::move(url));
   }
-  virtual ~RemoteProcessorGroupPort() = default;
+  virtual ~RemoteProcessGroupPort() = default;
 
   MINIFIAPI static constexpr auto hostName = core::PropertyDefinitionBuilder<>::createProperty("Host Name")
       .withDescription("Remote Host Name.")
@@ -122,9 +118,8 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
       idleTimeout
   });
 
-
-  MINIFIAPI static constexpr auto relation = core::RelationshipDefinition{"", ""};
-  MINIFIAPI static constexpr auto Relationships = std::array{relation};
+  MINIFIAPI static constexpr auto DefaultRelationship = core::RelationshipDefinition{"undefined", ""};
+  MINIFIAPI static constexpr auto Relationships = std::array{DefaultRelationship};
 
   MINIFIAPI static constexpr bool SupportsDynamicProperties = false;
   MINIFIAPI static constexpr bool SupportsDynamicRelationships = false;
@@ -139,12 +134,19 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
 
   void setDirection(sitetosite::TransferDirection direction) {
     direction_ = direction;
-    if (direction_ == sitetosite::RECEIVE)
-      this->setTriggerWhenEmpty(true);
+    if (direction_ == sitetosite::TransferDirection::RECEIVE) {
+      setTriggerWhenEmpty(true);
+    } else {
+      setTriggerWhenEmpty(false);
+    }
   }
 
-  void setTimeout(uint64_t timeout) {
+  void setTimeout(std::chrono::milliseconds timeout) {
     timeout_ = timeout;
+  }
+
+  std::optional<std::chrono::milliseconds> getTimeout() const {
+    return timeout_;
   }
 
   void setTransmitting(bool val) {
@@ -155,102 +157,90 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
     local_network_interface_ = ifc;
   }
 
-  std::string getInterface() {
-    return local_network_interface_;
-  }
-
-  /**
-   * Sets the url. Supports a CSV
-   */
-  void setURL(std::string val) {
-    auto urls = utils::string::split(val, ",");
-    for (const auto& url : urls) {
-      http::URL parsed_url{utils::string::trim(url)};
-      if (parsed_url.isValid()) {
-        logger_->log_debug("Parsed RPG URL '{}' -> '{}'", url, parsed_url.hostPort());
-        nifi_instances_.push_back({parsed_url.host(), parsed_url.port(), parsed_url.protocol()});
-      } else {
-        logger_->log_error("Could not parse RPG URL '{}'", url);
-      }
-    }
-  }
+  void setURL(const std::string& val);
 
   std::vector<RPG> getInstances() const {
     return nifi_instances_;
   }
 
   void setHTTPProxy(const http::HTTPProxy &proxy) {
-    this->proxy_ = proxy;
+    proxy_ = proxy;
   }
-  http::HTTPProxy getHTTPProxy() {
-    return this->proxy_;
-  }
-  // refresh remoteSite2SiteInfo via nifi rest api
-  std::pair<std::string, int> refreshRemoteSite2SiteInfo();
 
-  // refresh site2site peer list
-  void refreshPeerList();
+  http::HTTPProxy getHTTPProxy() {
+    return proxy_;
+  }
 
   void notifyStop() override;
 
   void enableHTTP() {
-    client_type_ = sitetosite::HTTP;
+    client_type_ = sitetosite::ClientType::HTTP;
+  }
+
+  void setUseCompression(bool use_compression) {
+    use_compression_ = use_compression;
+  }
+
+  bool getUseCompression() const {
+    return use_compression_;
+  }
+
+  void setBatchCount(uint64_t count) {
+    batch_count_ = count;
+  }
+
+  std::optional<uint64_t> getBatchCount() const {
+    return batch_count_;
+  }
+
+  void setBatchSize(uint64_t size) {
+    batch_size_ = size;
+  }
+
+  std::optional<uint64_t> getBatchSize() const {
+    return batch_size_;
+  }
+
+  void setBatchDuration(std::chrono::milliseconds duration) {
+    batch_duration_ = duration;
+  }
+
+  std::optional<std::chrono::milliseconds> getBatchDuration() const {
+    return batch_duration_;
   }
 
  protected:
-  /**
-   * Non static in case anything is loaded when this object is re-scheduled
-   */
-  bool is_http_disabled() {
-    auto ptr = core::ClassLoader::getDefaultClassLoader().instantiateRaw("HTTPClient", "HTTPClient");
-    if (ptr != nullptr) {
-      delete ptr;
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  std::unique_ptr<sitetosite::SiteToSiteClient> getNextProtocol(bool create);
+  std::optional<std::pair<std::string, uint16_t>> refreshRemoteSiteToSiteInfo();
+  void refreshPeerList();
+  std::unique_ptr<sitetosite::SiteToSiteClient> getNextProtocol();
   void returnProtocol(std::unique_ptr<sitetosite::SiteToSiteClient> protocol);
 
   moodycamel::ConcurrentQueue<std::unique_ptr<sitetosite::SiteToSiteClient>> available_protocols_;
-
   std::shared_ptr<Configure> configure_;
-  // Transaction Direction
   sitetosite::TransferDirection direction_;
-  // Transmitting
   std::atomic<bool> transmitting_;
-  // timeout
-  uint64_t timeout_;
-  // local network interface
+  std::optional<std::chrono::milliseconds> timeout_;
   std::string local_network_interface_;
-
   utils::Identifier protocol_uuid_;
-
-  std::chrono::milliseconds idle_timeout_ = std::chrono::seconds(15);
-
-  // rest API end point info
-  std::vector<struct RPG> nifi_instances_;
-
-  // http proxy
+  std::chrono::milliseconds idle_timeout_ = 15s;
+  std::vector<RPG> nifi_instances_;
   http::HTTPProxy proxy_;
-
-  bool bypass_rest_api_;
-
-  sitetosite::CLIENT_TYPE client_type_;
-
-  // Remote Site2Site Info
-  bool site2site_secure_;
+  sitetosite::ClientType client_type_;
   std::vector<sitetosite::PeerStatus> peers_;
-  std::atomic<int> peer_index_;
+  std::atomic<int64_t> peer_index_;
   std::mutex peer_mutex_;
-  std::string rest_user_name_;
-  std::string rest_password_;
-
-  std::shared_ptr<controllers::SSLContextService> ssl_service;
+  std::shared_ptr<controllers::SSLContextService> ssl_service_;
+  bool use_compression_{false};
+  std::optional<uint64_t> batch_count_;
+  std::optional<uint64_t> batch_size_;
+  std::optional<std::chrono::milliseconds> batch_duration_;
 
  private:
+  std::unique_ptr<sitetosite::SiteToSiteClient> initializeProtocol(sitetosite::SiteToSiteClientConfiguration& config) const;
+  std::optional<std::string> getRestApiToken(const RPG& nifi) const;
+  std::optional<std::pair<std::string, uint16_t>> parseSiteToSiteDataFromControllerConfig(const RPG& nifi, const std::string& controller) const;
+  std::optional<std::pair<std::string, uint16_t>> tryRefreshSiteToSiteInstance(RPG nifi) const;
+
   std::shared_ptr<core::logging::Logger> logger_;
   static const char* RPG_SSL_CONTEXT_SERVICE_NAME;
 };

libminifi/src/core/flow/StructuredConnectionParser.cpp
@@ -19,6 +19,7 @@
 #include "core/flow/StructuredConnectionParser.h"
 #include "core/flow/CheckRequiredField.h"
 #include "Funnel.h"
+#include "RemoteProcessGroupPort.h"
 
 namespace org::apache::nifi::minifi::core::flow {
 
@@ -28,7 +29,7 @@ void StructuredConnectionParser::addNewRelationshipToConnection(std::string_view
   connection.addRelationship(relationship);
 }
 
-void StructuredConnectionParser::addFunnelRelationshipToConnection(minifi::Connection& connection) const {
+void StructuredConnectionParser::addFunnelOrPortRelationshipToConnection(minifi::Connection& connection) const {
   utils::Identifier srcUUID;
   try {
     srcUUID = getSourceUUID();
@@ -37,13 +38,17 @@ void StructuredConnectionParser::addFunnelRelationshipToConnection(minifi::Conne
   }
   auto processor = parent_->findProcessorById(srcUUID);
   if (!processor) {
-    logger_->log_error("Could not find processor with id {}", srcUUID.to_string());
+    logger_->log_error("Could not find funnel or port with id {}", srcUUID.to_string());
     return;
   }
 
   auto& processor_ref = *processor;
   if (typeid(minifi::Funnel) == typeid(processor_ref)) {
     addNewRelationshipToConnection(minifi::Funnel::Success.name, connection);
+  } else if (typeid(minifi::RemoteProcessGroupPort) == typeid(processor_ref)) {
+    addNewRelationshipToConnection(minifi::RemoteProcessGroupPort::DefaultRelationship.name, connection);
+  } else {
+    logger_->log_error("Component with id {} is neither funnel nor port", srcUUID.to_string());
   }
 }
 
@@ -59,10 +64,10 @@ void StructuredConnectionParser::configureConnectionSourceRelationships(minifi::
     } else if (!relList.isSequence() && !relList.getString().value().empty()) {
       addNewRelationshipToConnection(relList.getString().value(), connection);
     } else {
-      addFunnelRelationshipToConnection(connection);
+      addFunnelOrPortRelationshipToConnection(connection);
     }
   } else {
-    addFunnelRelationshipToConnection(connection);
+    addFunnelOrPortRelationshipToConnection(connection);
   }
 }
 

EchoCog/minifi-cpp

libminifi/include/RemoteProcessGroupPort.h
@@ -1,6 +1,6 @@
 /**
- * @file RemoteProcessorGroupPort.h
- * RemoteProcessorGroupPort class declaration
+ * @file RemoteProcessGroupPort.h
+ * RemoteProcessGroupPort class declaration
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -42,57 +42,26 @@
 
 namespace org::apache::nifi::minifi {
 
-/**
- * Count down latch implementation that's used across
- * all threads of the RPG. This is okay since the latch increments
- * and decrements based on its construction. Using RAII we should
- * never have the concern of thread safety.
- */
-class RPGLatch {
- public:
-  RPGLatch(bool increment = true) { // NOLINT
-    static std::atomic<int> latch_count(0);
-    count = &latch_count;
-    if (increment)
-      count++;
-  }
-
-  ~RPGLatch() {
-    count--;
-  }
-
-  int getCount() {
-    return *count;
-  }
-
- private:
-  std::atomic<int> *count;
-};
-
 struct RPG {
-  std::string host_;
-  int port_;
-  std::string protocol_;
+  std::string host;
+  int port;
+  std::string protocol;
 };
 
-class RemoteProcessorGroupPort : public core::ProcessorImpl {
+class RemoteProcessGroupPort : public core::ProcessorImpl {
  public:
-  RemoteProcessorGroupPort(std::string_view name, std::string url, std::shared_ptr<Configure> configure, const utils::Identifier &uuid, std::shared_ptr<core::logging::Logger> logger = nullptr)
-      : core::ProcessorImpl({.uuid = uuid, .name = std::string{name}, .logger = logger ? logger : core::logging::LoggerFactory<RemoteProcessorGroupPort>::getLogger(uuid)}),
+  RemoteProcessGroupPort(std::string_view name, std::string url, std::shared_ptr<Configure> configure, const utils::Identifier &uuid, std::shared_ptr<core::logging::Logger> logger = nullptr)
+      : core::ProcessorImpl({.uuid = uuid, .name = std::string{name}, .logger = logger ? logger : core::logging::LoggerFactory<RemoteProcessGroupPort>::getLogger(uuid)}),
         configure_(std::move(configure)),
-        direction_(sitetosite::SEND),
+        direction_(sitetosite::TransferDirection::SEND),
         transmitting_(false),
-        timeout_(0),
-        bypass_rest_api_(false),
-        ssl_service(nullptr) {
-    client_type_ = sitetosite::CLIENT_TYPE::RAW;
-    protocol_uuid_ = uuid;
-    site2site_secure_ = false;
-    peer_index_ = -1;
+        protocol_uuid_(uuid),
+        client_type_(sitetosite::ClientType::RAW),
+        peer_index_(-1) {
     // REST API port and host
     setURL(std::move(url));
   }
-  virtual ~RemoteProcessorGroupPort() = default;
+  virtual ~RemoteProcessGroupPort() = default;
 
   MINIFIAPI static constexpr auto hostName = core::PropertyDefinitionBuilder<>::createProperty("Host Name")
       .withDescription("Remote Host Name.")
@@ -121,9 +90,8 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
       idleTimeout
   });
 
-
-  MINIFIAPI static constexpr auto relation = core::RelationshipDefinition{"", ""};
-  MINIFIAPI static constexpr auto Relationships = std::array{relation};
+  MINIFIAPI static constexpr auto DefaultRelationship = core::RelationshipDefinition{"undefined", ""};
+  MINIFIAPI static constexpr auto Relationships = std::array{DefaultRelationship};
 
   MINIFIAPI static constexpr bool SupportsDynamicProperties = false;
   MINIFIAPI static constexpr bool SupportsDynamicRelationships = false;
@@ -138,12 +106,19 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
 
   void setDirection(sitetosite::TransferDirection direction) {
     direction_ = direction;
-    if (direction_ == sitetosite::RECEIVE)
-      this->setTriggerWhenEmpty(true);
+    if (direction_ == sitetosite::TransferDirection::RECEIVE) {
+      setTriggerWhenEmpty(true);
+    } else {
+      setTriggerWhenEmpty(false);
+    }
   }
 
-  void setTimeout(uint64_t timeout) {
+  void setTimeout(std::chrono::milliseconds timeout) {
     timeout_ = timeout;
+  }
+
+  [[nodiscard]] std::optional<std::chrono::milliseconds> getTimeout() const {
+    return timeout_;
   }
 
   void setTransmitting(bool val) {
@@ -154,102 +129,90 @@ class RemoteProcessorGroupPort : public core::ProcessorImpl {
     local_network_interface_ = ifc;
   }
 
-  std::string getInterface() {
-    return local_network_interface_;
-  }
+  void setURL(const std::string& val);
 
-  /**
-   * Sets the url. Supports a CSV
-   */
-  void setURL(std::string val) {
-    auto urls = utils::string::split(val, ",");
-    for (const auto& url : urls) {
-      http::URL parsed_url{utils::string::trim(url)};
-      if (parsed_url.isValid()) {
-        logger_->log_debug("Parsed RPG URL '{}' -> '{}'", url, parsed_url.hostPort());
-        nifi_instances_.push_back({parsed_url.host(), parsed_url.port(), parsed_url.protocol()});
-      } else {
-        logger_->log_error("Could not parse RPG URL '{}'", url);
-      }
-    }
-  }
-
-  std::vector<RPG> getInstances() const {
+  [[nodiscard]] std::vector<RPG> getInstances() const {
     return nifi_instances_;
   }
 
   void setHTTPProxy(const http::HTTPProxy &proxy) {
-    this->proxy_ = proxy;
+    proxy_ = proxy;
   }
-  http::HTTPProxy getHTTPProxy() {
-    return this->proxy_;
-  }
-  // refresh remoteSite2SiteInfo via nifi rest api
-  std::pair<std::string, int> refreshRemoteSite2SiteInfo();
 
-  // refresh site2site peer list
-  void refreshPeerList();
+  [[nodiscard]] http::HTTPProxy getHTTPProxy() const {
+    return proxy_;
+  }
 
   void notifyStop() override;
 
   void enableHTTP() {
-    client_type_ = sitetosite::HTTP;
+    client_type_ = sitetosite::ClientType::HTTP;
+  }
+
+  void setUseCompression(bool use_compression) {
+    use_compression_ = use_compression;
+  }
+
+  [[nodiscard]] bool getUseCompression() const {
+    return use_compression_;
+  }
+
+  void setBatchCount(uint64_t count) {
+    batch_count_ = count;
+  }
+
+  [[nodiscard]] std::optional<uint64_t> getBatchCount() const {
+    return batch_count_;
+  }
+
+  void setBatchSize(uint64_t size) {
+    batch_size_ = size;
+  }
+
+  [[nodiscard]] std::optional<uint64_t> getBatchSize() const {
+    return batch_size_;
+  }
+
+  void setBatchDuration(std::chrono::milliseconds duration) {
+    batch_duration_ = duration;
+  }
+
+  [[nodiscard]] std::optional<std::chrono::milliseconds> getBatchDuration() const {
+    return batch_duration_;
   }
 
  protected:
-  /**
-   * Non static in case anything is loaded when this object is re-scheduled
-   */
-  bool is_http_disabled() {
-    auto ptr = core::ClassLoader::getDefaultClassLoader().instantiateRaw("HTTPClient", "HTTPClient");
-    if (ptr != nullptr) {
-      delete ptr;
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  std::unique_ptr<sitetosite::SiteToSiteClient> getNextProtocol(bool create);
+  std::optional<std::pair<std::string, uint16_t>> refreshRemoteSiteToSiteInfo();
+  void refreshPeerList();
+  std::unique_ptr<sitetosite::SiteToSiteClient> getNextProtocol();
   void returnProtocol(core::ProcessContext& context, std::unique_ptr<sitetosite::SiteToSiteClient> protocol);
 
   moodycamel::ConcurrentQueue<std::unique_ptr<sitetosite::SiteToSiteClient>> available_protocols_;
-
   std::shared_ptr<Configure> configure_;
-  // Transaction Direction
   sitetosite::TransferDirection direction_;
-  // Transmitting
   std::atomic<bool> transmitting_;
-  // timeout
-  uint64_t timeout_;
-  // local network interface
+  std::optional<std::chrono::milliseconds> timeout_;
   std::string local_network_interface_;
-
   utils::Identifier protocol_uuid_;
-
-  std::chrono::milliseconds idle_timeout_ = std::chrono::seconds(15);
-
-  // rest API end point info
-  std::vector<struct RPG> nifi_instances_;
-
-  // http proxy
+  std::chrono::milliseconds idle_timeout_ = 15s;
+  std::vector<RPG> nifi_instances_;
   http::HTTPProxy proxy_;
-
-  bool bypass_rest_api_;
-
-  sitetosite::CLIENT_TYPE client_type_;
-
-  // Remote Site2Site Info
-  bool site2site_secure_;
+  sitetosite::ClientType client_type_;
   std::vector<sitetosite::PeerStatus> peers_;
-  std::atomic<int> peer_index_;
+  int64_t peer_index_;
   std::mutex peer_mutex_;
-  std::string rest_user_name_;
-  std::string rest_password_;
-
-  std::shared_ptr<controllers::SSLContextServiceInterface> ssl_service;
+  std::shared_ptr<controllers::SSLContextServiceInterface> ssl_service_;
+  bool use_compression_{false};
+  std::optional<uint64_t> batch_count_;
+  std::optional<uint64_t> batch_size_;
+  std::optional<std::chrono::milliseconds> batch_duration_;
 
  private:
+  gsl::not_null<std::unique_ptr<sitetosite::SiteToSiteClient>> initializeProtocol(sitetosite::SiteToSiteClientConfiguration& config) const;
+  [[nodiscard]] std::optional<std::string> getRestApiToken(const RPG& nifi) const;
+  std::optional<std::pair<std::string, uint16_t>> parseSiteToSiteDataFromControllerConfig(const RPG& nifi, const std::string& controller) const;
+  std::optional<std::pair<std::string, uint16_t>> tryRefreshSiteToSiteInstance(RPG nifi) const;
+
   static const char* RPG_SSL_CONTEXT_SERVICE_NAME;
 };
 

libminifi/src/core/flow/StructuredConnectionParser.cpp
@@ -19,6 +19,7 @@
 #include "core/flow/StructuredConnectionParser.h"
 #include "core/flow/CheckRequiredField.h"
 #include "Funnel.h"
+#include "RemoteProcessGroupPort.h"
 
 namespace org::apache::nifi::minifi::core::flow {
 
@@ -28,7 +29,7 @@ void StructuredConnectionParser::addNewRelationshipToConnection(std::string_view
   connection.addRelationship(relationship);
 }
 
-void StructuredConnectionParser::addFunnelRelationshipToConnection(minifi::Connection& connection) const {
+void StructuredConnectionParser::addFunnelOrPortRelationshipToConnection(minifi::Connection& connection) const {
   utils::Identifier srcUUID;
   try {
     srcUUID = getSourceUUID();
@@ -37,13 +38,17 @@ void StructuredConnectionParser::addFunnelRelationshipToConnection(minifi::Conne
   }
   auto processor = parent_->findProcessorById(srcUUID);
   if (!processor) {
-    logger_->log_error("Could not find processor with id {}", srcUUID.to_string());
+    logger_->log_error("Could not find funnel or port with id {}", srcUUID.to_string());
     return;
   }
 
   auto& impl = processor->getImpl();
   if (typeid(minifi::Funnel) == typeid(impl)) {
     addNewRelationshipToConnection(minifi::Funnel::Success.name, connection);
+  } else if (typeid(minifi::RemoteProcessGroupPort) == typeid(impl)) {
+    addNewRelationshipToConnection(minifi::RemoteProcessGroupPort::DefaultRelationship.name, connection);
+  } else {
+    logger_->log_error("Component with id {} is neither funnel nor port", srcUUID.to_string());
   }
 }
 
@@ -59,10 +64,10 @@ void StructuredConnectionParser::configureConnectionSourceRelationships(minifi::
     } else if (!relList.isSequence() && !relList.getString().value().empty()) {
       addNewRelationshipToConnection(relList.getString().value(), connection);
     } else {
-      addFunnelRelationshipToConnection(connection);
+      addFunnelOrPortRelationshipToConnection(connection);
     }
   } else {
-    addFunnelRelationshipToConnection(connection);
+    addFunnelOrPortRelationshipToConnection(connection);
   }
 }
 